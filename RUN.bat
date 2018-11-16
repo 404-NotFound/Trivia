9eea93dbad649bf5c195554788b34c7f3a596ac3
matimutter <44354217+matimutter@users.noreply.github.com>
2018-11-16 06:43:30 -0300

    batch

--- RUN.bat
+++ RUN.bat
@@ -5,7 +5,6 @@ color 8f
 for /f "delims=[] tokens=2" %%a in ('ping -4 -n 1 %ComputerName% ^| findstr [') do set NetworkIP=%%a
 for /f %%a in ('powershell Invoke-RestMethod api.ipify.org') do set PublicIP=%%a
 TITLE Flask App: TRIVIA   -   ( IP Privada: %NetworkIP% )   -   ( IP Publica: %PublicIP% )
-::Carpeta de app.py
 cd C:\eclipse-workspace\Trivia
 set FLASK_APP=app.py
 set FLASK_ENV=development
